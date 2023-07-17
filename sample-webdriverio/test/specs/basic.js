45977f12deda8c10eb03d02388257793d4b19f9a
Anna Sukhodolova <asukhodolova@solvd.com>
2023-07-17 09:47:03 +0300

    Webdriverio demo

--- sample-webdriverio/test/specs/basic.js
+++ sample-webdriverio/test/specs/basic.js
@@ -1,5 +1,5 @@
 const log = require('loglevel');
-const { currentTest } = require('@zebrunner/javascript-agent-webdriverio');
+const { currentTest, zebrunner } = require('@zebrunner/javascript-agent-webdriverio');
 const { Key } = require('webdriverio');
 
 describe('Basic reporting test', () => {
@@ -16,19 +16,38 @@ describe('Basic reporting test', () => {
     logger.info(`Test ${this.currentTest.title} started`);
   });
 
-  it('Google search', async () => {
+  it('Google search should be passed', async () => {
+    zebrunner.testCaseKey('ZPT-3010', 'ZPT-3679');
+
     logger.info(`Opening ${url}`);
     await browser.url(url);
     currentTest.saveScreenshot(await browser.takeScreenshot());
 
     logger.info(`Performing search with value: ${searchValue}`);
-    await $("//input[@name='q']").setValue(searchValue);
+    await $("//*[@name='q']").setValue(searchValue);
     await browser.keys(Key.Enter);
 
     logger.info(`Verify first search result contains ${searchValue}`);
     currentTest.saveScreenshot(await browser.takeScreenshot());
 
     await expect($("//*[@id='search']//a")).toHaveTextContaining(searchValue);
+  });
+
+  it('Google search should be failed', async () => {
+    zebrunner.testCaseKey('ZPT-3011', 'ZPT-3680');
+
+    logger.info(`Opening ${url}`);
+    await browser.url(url);
+    currentTest.saveScreenshot(await browser.takeScreenshot());
+
+    logger.info(`Performing search with value: ${searchValue}`);
+    await $("//*[@name='q']").setValue(searchValue);
+    await browser.keys(Key.Enter);
+
+    logger.info(`Verify first search result equals ${searchValue}`);
+    currentTest.saveScreenshot(await browser.takeScreenshot());
+
+    await expect($("//*[@id='search']//a")).toHaveText(searchValue);
   });
 
   afterEach(function () {
