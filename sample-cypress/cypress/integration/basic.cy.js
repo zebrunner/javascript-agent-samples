9065d54fc097c5df7a023068c12762556b190374
Anna Sukhodolova <asukhodolova@solvd.com>
2023-07-17 09:45:26 +0300

    Cypress demo

--- sample-cypress/cypress/integration/basic.cy.js
+++ sample-cypress/cypress/integration/basic.cy.js
@@ -2,13 +2,27 @@ describe('Google search', () => {
   const url = 'https://www.google.com/';
   const searchValue = 'Zebrunner';
 
-  it('Should return search results', () => {
+  it('Should pass', { 'owner': 'asukhodolova' }, () => {
+    cy.zebrunnerTestCaseKey('ZPT-3010', 'ZPT-3679');
+
     cy.visit(url).contains('Google');
 
     console.log(`Performing search with value Zebrunner`);
-    cy.xpath("//input[@name='q']").click().type(searchValue).type('{enter}');
+    cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
 
     console.log(`Verify first search result contains search value`);
     cy.xpath("//*[@id='search']//a").should('contain.text', searchValue);
+  });
+
+  it('Should fail', () => {
+    cy.zebrunnerTestCaseKey('ZPT-3011', 'ZPT-3680');
+
+    cy.visit(url).contains('Google');
+
+    console.log(`Performing search with value Zebrunner`);
+    cy.xpath("//*[@name='q']").click().type(searchValue).type('{enter}');
+
+    console.log(`Verify first search result contains search value`);
+    cy.xpath("//*[@id='search']//a").should('contain.text', "some unexisting text");
   });
 });
